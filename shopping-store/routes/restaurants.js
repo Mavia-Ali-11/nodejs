59f8c46b26747b82530eec4eb4ebb3f007782b93
Mavia Ali <maviaali597@gmail.com>
2024-04-28 00:46:29 +0500

    2 new APIs created

--- shopping-store/routes/restaurants.js
+++ shopping-store/routes/restaurants.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { getAllRestaurants, getRestaurantsMeta, getBoroughSpecific, getRestaurantAgainstScores } = require("../controllers/restaurants");
+const {
+    getAllRestaurants,
+    getRestaurantsMeta,
+    getBoroughSpecific,
+    getAgainstScores,
+    getAgainstLocation,
+    getAgainstSearch
+} = require("../controllers/restaurants");
 
 router.get("/", getAllRestaurants);
 router.get("/meta", getRestaurantsMeta);
 router.get("/borough", getBoroughSpecific);
-router.get("/score/:score", getRestaurantAgainstScores);
+router.get("/score/:score", getAgainstScores);
+router.get("/location", getAgainstLocation);
+router.get("/search", getAgainstSearch);
 
 module.exports = router;
